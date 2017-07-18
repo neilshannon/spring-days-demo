3a1cb64dbac4462e058ec1291480a6ea96a01e7e
neilshannon <neil@nts-dev.com>
2017-07-18 08:25:04 -0400

    minor updates

--- src/main/java/com/ntsdev/repository/PersonRepository.java
+++ src/main/java/com/ntsdev/repository/PersonRepository.java
@@ -9,7 +9,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
 /**
- * Sets up a Spring Data MongoDB repository for the `people` collection and a REST endpoint.
+ * Sets up a Spring Data MongoDB repository for the `person` collection and a REST endpoint
+ * at /people.
  */
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends MongoRepository<Person, String> {
